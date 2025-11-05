508b646f1c2059dc576100423ace09b2edf028de
aliza-rizwan <aliza.rizwan@ontariotechu.net>
2025-11-05 00:58:16 -0500

    Update main.py

--- main.py
+++ main.py
@@ -22,11 +22,30 @@ from db.models import *
 ############################################################################
 ## START OF APPLICATION
 ############################################################################
-""" Replace the code below with your own """
+from db.models import Product
 
-# Seed a few users in the database
-User.objects.create(name='Dan')
-User.objects.create(name='Robert')
+# Populate the databse if it is empty
+if not Product.objects.exists():
+    products = [
+        {"upc": "111111111111", "name": "Milk", "price": 2.99},
+        {"upc": "222222222222", "name": "Bread", "price": 1.79},
+        {"upc": "333333333333", "name": "Eggs", "price": 3.49},
+        {"upc": "444444444444", "name": "Cheese", "price": 5.20},
+        {"upc": "555555555555", "name": "Jam", "price": 3.24},
+        {"upc": "666666666666", "name": "Chicken", "price": 20.0},
+    ]
+    for p in products:
+        Product.objects.create(**p)
+    print("Database populated successfully!\n")
 
-for u in User.objects.all():
-    print(f'ID: {u.id} \tUsername: {u.name}')
+# Cash Register Scanning
+while True:
+    user_input = input("Enter product UPC (or 'exit' to quit): ").strip()
+    if user_input.lower() == "exit":
+        print("Exiting Cash Register. Goodbye!")
+        break
+    try:
+        product = Product.objects.get(upc=user_input)
+        print(f"Product: {product.name}\tPrice: ${product.price}\n")
+    except Product.DoesNotExist:
+        print("Product was not found. Please Try again.\n")
