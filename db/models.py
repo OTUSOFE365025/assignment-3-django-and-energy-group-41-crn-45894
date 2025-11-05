1d12dc2dd34c5f706f2513b589ab430b4cb0ca08
aliza-rizwan <aliza.rizwan@ontariotechu.net>
2025-11-05 00:59:30 -0500

    Update models.py

--- db/models.py
+++ db/models.py
@@ -14,3 +14,12 @@ class User(models.Model):
     def __str__(self):
         return self.name
 
+class Product(models.Model):
+    upc = models.CharField(max_length=12, unique=True)
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.name} - ${self.price}"
+
+
